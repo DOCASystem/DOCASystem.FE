0aa2e4a1cea6ccd4df888259a0b7697f5d18832f
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2025-02-12 10:05:35 +0700

    add 'cn' update footer

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/footer";
 import Header from "@/components/header";
 
 export default function Home() {
   return (
     <div className="container w-[1296px] mx-auto mt-3">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,12 @@
+import LogoDoca from "../sections/logo/logo-doca";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="flex flex-col">
+        <LogoDoca />
+        <p>Hành trình mang yêu thương cho những mảnh đời “nhỏ bé”</p>
+      </div>
+    </footer>
+  );
+}
