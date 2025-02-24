9021dfeb4d002d3b0f70ccdced39d857c4e5326a
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2025-02-24 15:25:03 +0700

    add filter to shop page

--- src/components/common/checkbox/checkbox.tsx
+++ src/components/common/checkbox/checkbox.tsx
@@ -3,25 +3,45 @@ import { cn } from "@/utils/cn";
 interface CheckboxProps {
   label: string;
   checked: boolean;
-  className?: string;
+  itemCount?: number;
+  classNameInput?: string;
+  classNameLabel?: string;
+  classNameItemCount?: string;
+
   onChange: () => void;
 }
 
 export default function Checkbox({
   label,
   checked,
-  className,
+  itemCount,
+  classNameInput,
+  classNameLabel,
+  classNameItemCount,
   onChange,
 }: CheckboxProps) {
   return (
-    <label className="flex items-center space-x-2">
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={onChange}
-        className={cn("w-5 h-5 accent-slate-500", className)}
-      />
-      <span className={(cn(), className)}>{label}</span>
+    <label className="flex justify-between items-center">
+      <div className="flex justify-center items-center gap-2">
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+          className={cn(
+            "w-4 h-4 accent-slate-500 border-2 text-[16px] font-medium",
+            classNameInput
+          )}
+        />
+        <span className={cn("", classNameLabel)}>{label}</span>
+      </div>
+      <span
+        className={cn(
+          "py-1 px-2 flex items-center justify-center text-pink-doca rounded-xl bg-gray-100",
+          classNameItemCount
+        )}
+      >
+        {itemCount}
+      </span>
     </label>
   );
 }
