b55080df3420eb4e0514c2dec06afd11b7275f36
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2025-03-01 02:37:18 +0700

    update UI login: add image, fix btn

--- src/components/sections/auth/login/form-login.tsx
+++ src/components/sections/auth/login/form-login.tsx
@@ -5,6 +5,8 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "@/utils/validation";
 import Input from "@/components/common/input/input";
 import Button from "@/components/common/button/button";
+import Image from "next/image";
+import LinkNav from "@/components/common/link/link";
 
 interface FormData {
   email: string;
@@ -22,26 +24,58 @@ export default function LoginForm() {
   });
 
   return (
-    <FormProvider {...methods}>
-      <form
-        onSubmit={onSubmit}
-        className="bg-gray-100 p-6 rounded-xl shadow-md min-w-[550px]"
-      >
-        <h1 className="text-2xl font-semibold text-center">Đăng nhập</h1>
-        <Input name="email" label="Email" placeholder="Nhập email" />
-        <Input
-          name="password"
-          label="Mật khẩu"
-          placeholder="Nhập mật khẩu"
-          type="password"
-        />
+    <div className="flex flex-row">
+      <FormProvider {...methods}>
+        <form
+          onSubmit={onSubmit}
+          className="bg-gray-100 flex flex-col justify-center items-center w-[1536px] p-6 rounded-xl shadow-md min-w-[550px]"
+        >
+          <h1 className="text-2xl font-semibold">Đăng nhập</h1>
+          <div className="flex flex-col gap-4 mt-6">
+            <Input
+              name="email"
+              label="Email"
+              placeholder="Nhập email"
+              className="w-[404px]"
+            />
+            <Input
+              name="password"
+              label="Mật khẩu"
+              placeholder="Nhập mật khẩu"
+              type="password"
+              className="w-[404px]"
+            />
 
-        <div className="mt-6">
-          <Button className="w-32 h-12 bg-pink-doca text-white rounded">
-            Gửi
-          </Button>
-        </div>
-      </form>
-    </FormProvider>
+            <LinkNav
+              href="/auth/forgot-password"
+              className="text-pink-doca font-semibold left-0 "
+            >
+              Quên mật khẩu?
+            </LinkNav>
+            <div className="mt-6 flex flex-col gap-4">
+              <Button className="h-12 w-[404px] bg-pink-doca text-white rounded-3xl text-[16px]">
+                Đăng Nhập
+              </Button>
+              <div className="flex gap-2 items-center justify-center">
+                <p>Bạn chưa có tài khoản? </p>
+                <LinkNav
+                  href="/auth/signup"
+                  className="text-pink-doca font-semibold left-0 text-right"
+                >
+                  Tạo tài khoản
+                </LinkNav>
+              </div>
+            </div>
+          </div>
+        </form>
+      </FormProvider>
+      <Image
+        src="/images/bg-login.png"
+        alt="bg-login"
+        width={600}
+        height={900}
+        className="w-[700px] h-[700px] rounded-l-2xl"
+      />
+    </div>
   );
 }
